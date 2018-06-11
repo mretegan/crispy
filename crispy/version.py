3c3ba6ed3c6aa9a35621aa72220bbea398dcd004
Marius Retegan <marius.s.retegan@gmail.com>
2018-06-11 11:11:04 +0200

    Update version number to 0.7.0

--- crispy/version.py
+++ crispy/version.py
@@ -64,9 +64,9 @@ RELEASE_LEVEL_VALUE = {'dev': 0,
                        'final': 15}
 
 MAJOR = 0
-MINOR = 6
-MICRO = 3
-RELEV = 'final'  # <16
+MINOR = 7
+MICRO = 0
+RELEV = 'dev'  # <16
 SERIAL = 0  # <16
 
 date = __date__
