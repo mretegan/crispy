bb15b1f719bb368c1b7422de36a330369b0bfbec
Marius Retegan <marius.s.retegan@gmail.com>
2018-06-15 13:35:10 +0200

    Add DenseBorder to 1s2p and 1s3p RIXS templates

--- crispy/resources/modules/quanty/templates/3d_1s3p_Td.lua
+++ crispy/resources/modules/quanty/templates/3d_1s3p_Td.lua
@@ -541,20 +541,22 @@ DeltaE2 = Eedge2 + E_gs_i - E_gs_f
 
 Emin1 = $Emin1 - DeltaE1
 Emax1 = $Emax1 - DeltaE1
-Gamma1 = $Gamma1
 NE1 = $NE1
+Gamma1 = $Gamma1
 
 Emin2 = $Emin2 - DeltaE2
 Emax2 = $Emax2 - DeltaE2
-Gamma2 = $Gamma2
 NE2 = $NE2
+Gamma2 = $Gamma2
+
+DenseBorder = $DenseBorder
 
 G = 0
 
 if CalculationRestrictions == nil then
-    G = G + CreateResonantSpectra(H_m, H_f, {Txy_1s_3d, Txz_1s_3d, Tyz_1s_3d, Tx2y2_1s_3d, Tz2_1s_3d}, {Tx_3p_1s, Ty_3p_1s, Tz_3p_1s}, Psis_i, {{'Emin1', Emin1}, {'Emax1', Emax1}, {'NE1', NE1}, {'Gamma1', Gamma1}, {'Emin2', Emin2}, {'Emax2', Emax2}, {'NE2', NE2}, {'Gamma2', Gamma2}})
+    G = G + CreateResonantSpectra(H_m, H_f, {Txy_1s_3d, Txz_1s_3d, Tyz_1s_3d, Tx2y2_1s_3d, Tz2_1s_3d}, {Tx_3p_1s, Ty_3p_1s, Tz_3p_1s}, Psis_i, {{'Emin1', Emin1}, {'Emax1', Emax1}, {'NE1', NE1}, {'Gamma1', Gamma1}, {'Emin2', Emin2}, {'Emax2', Emax2}, {'NE2', NE2}, {'Gamma2', Gamma2}, {'DenseBorder', DenseBorder}})
 else
-    G = G + CreateResonantSpectra(H_m, H_f, {Txy_1s_3d, Txz_1s_3d, Tyz_1s_3d, Tx2y2_1s_3d, Tz2_1s_3d}, {Tx_3p_1s, Ty_3p_1s, Tz_3p_1s}, Psis_i, {{'Emin1', Emin1}, {'Emax1', Emax1}, {'NE1', NE1}, {'Gamma1', Gamma1}, {'Emin2', Emin2}, {'Emax2', Emax2}, {'NE2', NE2}, {'Gamma2', Gamma2}, {'restrictions1', CalculationRestrictions}, {'restrictions2', CalculationRestrictions}})
+    G = G + CreateResonantSpectra(H_m, H_f, {Txy_1s_3d, Txz_1s_3d, Tyz_1s_3d, Tx2y2_1s_3d, Tz2_1s_3d}, {Tx_3p_1s, Ty_3p_1s, Tz_3p_1s}, Psis_i, {{'Emin1', Emin1}, {'Emax1', Emax1}, {'NE1', NE1}, {'Gamma1', Gamma1}, {'Emin2', Emin2}, {'Emax2', Emax2}, {'NE2', NE2}, {'Gamma2', Gamma2}, {'restrictions1', CalculationRestrictions}, {'restrictions2', CalculationRestrictions}, {'DenseBorder', DenseBorder}})
 end
 
 Gtot = 0
