6db0905f1c23c1c71916a34b2a7721f0bc45aad4
Marius Retegan <marius.s.retegan@gmail.com>
2020-06-04 22:26:45 +0200

    Update plotting module

--- crispy/gui/plot.py
+++ crispy/gui/plot.py
@@ -2,6 +2,8 @@
 ###################################################################
 # Copyright (c) 2016-2020 European Synchrotron Radiation Facility #
 #                                                                 #
+# Authors: Marius Retegan                                         #
+#                                                                 #
 # This work is licensed under the terms of the MIT license.       #
 # For further information, see https://github.com/mretegan/crispy #
 ###################################################################
@@ -9,13 +11,11 @@
 import sys
 
 import matplotlib.lines as mlines
-import numpy as np
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtWidgets import QMenu, QToolBar
 
-from silx.gui.plot import PlotWidget
+from silx.gui.plot import PlotWidget, items
 from silx.gui.plot.actions.control import (
     ColormapAction,
     CurveStyleAction,
@@ -26,130 +26,146 @@ from silx.gui.plot.actions.control import (
     YAxisAutoScaleAction,
     ZoomBackAction,
 )
-from silx.gui.plot.actions.mode import ZoomModeAction, PanModeAction
 from silx.gui.plot.actions.io import CopyAction, SaveAction
-from silx.gui.plot.items.curve import Curve
-from silx.gui.plot.items.image import ImageData
+from silx.gui.plot.actions.mode import PanModeAction, ZoomModeAction
+from silx.gui.plot.tools.profile.manager import ProfileManager, ProfileWindow
+from silx.gui.plot.tools.profile.rois import (
+    ProfileImageCrossROI,
+    ProfileImageHorizontalLineROI,
+    ProfileImageLineROI,
+    ProfileImageVerticalLineROI,
+)
 
-__authors__ = ["Marius Retegan"]
-__license__ = "MIT"
-__date__ = "13/05/2019"
+
+class CustomProfileWindow(ProfileWindow):
+    def __init__(self, parent=None, backend=None):
+        super().__init__(parent=parent, backend=backend)
+
+        self.setWindowTitle(str())
+
+    @staticmethod
+    def createPlot1D(parent, backend):
+        plot = CustomPlotWidget(parent=parent, backend=backend)
+        return plot
 
 
-class BasePlotWidget(PlotWidget):  # pylint: disable=too-many-instance-attributes
-    SNAP_THRESHOLD_DIST = 5
+class CustomPlotWidget(PlotWidget):  # pylint: disable=too-many-instance-attributes
+    def __init__(self, parent=None, backend="matplotlib"):
+        super().__init__(parent=parent, backend=backend)
 
-    def __init__(self, parent=None, **kwargs):
-        super(BasePlotWidget, self).__init__(
-            parent=parent, backend="matplotlib", **kwargs
-        )
+        self.plotArea = self.getWidgetHandle()
+
+        if sys.platform == "darwin":
+            self.setIconSize(QSize(24, 24))
 
         self.setActiveCurveHandling(False)
         self.setGraphGrid("both")
 
-        # Add the interactive mode toolbar.
-        self.interactionModeToolBar = QToolBar("Interaction", parent=self)
+        self.addInteractiveToolBar()
+        self.addMainToolBar()
+        self.addProfileToolBar()
+        self.addOutputToolBar()
 
-        self.zoomModeAction = ZoomModeAction(self, parent=self.interactionModeToolBar)
-        self.interactionModeToolBar.addAction(self.zoomModeAction)
+        self.sigContentChanged.connect(self.setProfileToolBarVisibility)
 
-        self.panModeAction = PanModeAction(self, parent=self.interactionModeToolBar)
-        self.interactionModeToolBar.addAction(self.panModeAction)
+    def addInteractiveToolBar(self):
+        self.interactiveToolBar = QToolBar("Interaction", parent=self)
 
-        self.addToolBar(self.interactionModeToolBar)
+        zoomModeAction = ZoomModeAction(plot=self, parent=self.interactiveToolBar)
+        panModeAction = PanModeAction(plot=self, parent=self.interactiveToolBar)
 
-        # Add a custom toolbar for curves and images.
+        self.interactiveToolBar.addAction(zoomModeAction)
+        self.interactiveToolBar.addAction(panModeAction)
+
+        self.addToolBar(self.interactiveToolBar)
+
+    def addMainToolBar(self):
         self.mainToolBar = QToolBar("Curve or Image", parent=self)
 
-        self.resetZoomAction = ResetZoomAction(self, parent=self.mainToolBar)
+        self.resetZoomAction = ResetZoomAction(plot=self, parent=self.mainToolBar)
+        self.xAxisAutoScaleAction = XAxisAutoScaleAction(
+            plot=self, parent=self.mainToolBar
+        )
+        self.yAxisAutoScaleAction = YAxisAutoScaleAction(
+            plot=self, parent=self.mainToolBar
+        )
+        self.curveStyleAction = CurveStyleAction(plot=self, parent=self.mainToolBar)
+        self.colormapAction = ColormapAction(plot=self, parent=self.mainToolBar)
+        self.keepAspectRatioAction = KeepAspectRatioAction(
+            plot=self, parent=self.mainToolBar
+        )
+        self.gridAction = GridAction(plot=self, parent=self.mainToolBar)
+
         self.mainToolBar.addAction(self.resetZoomAction)
-
-        self.xAxisAutoScaleAction = XAxisAutoScaleAction(self, parent=self.mainToolBar)
         self.mainToolBar.addAction(self.xAxisAutoScaleAction)
-
-        self.yAxisAutoScaleAction = YAxisAutoScaleAction(self, parent=self.mainToolBar)
         self.mainToolBar.addAction(self.yAxisAutoScaleAction)
-
-        self.gridAction = GridAction(self, parent=self.mainToolBar)
         self.mainToolBar.addAction(self.gridAction)
-
-        self.curveStyleAction = CurveStyleAction(self, parent=self.mainToolBar)
         self.mainToolBar.addAction(self.curveStyleAction)
-
-        self.colormapAction = ColormapAction(self, parent=self.mainToolBar)
         self.mainToolBar.addAction(self.colormapAction)
-
-        self.keepAspectRatio = KeepAspectRatioAction(self, parent=self.mainToolBar)
-        self.mainToolBar.addAction(self.keepAspectRatio)
+        self.mainToolBar.addAction(self.keepAspectRatioAction)
 
         self.addToolBar(self.mainToolBar)
 
-        # Add the output toolbar.
+    def addOutputToolBar(self):
         self.outputToolBar = QToolBar("IO", parent=self)
 
-        self.copyAction = CopyAction(self, parent=self.outputToolBar)
-        self.outputToolBar.addAction(self.copyAction)
+        self.copyAction = CopyAction(plot=self, parent=self.outputToolBar)
+        self.saveAction = SaveAction(plot=self, parent=self.outputToolBar)
 
-        self.saveAction = SaveAction(self, parent=self.outputToolBar)
+        self.outputToolBar.addAction(self.copyAction)
         self.outputToolBar.addAction(self.saveAction)
 
         self.addToolBar(self.outputToolBar)
 
-        windowHandle = self.window().windowHandle()
-        if windowHandle is not None:
-            self.ratio = windowHandle.devicePixelRatio()
+    def addProfileToolBar(self):
+        self.profileToolBar = QToolBar("Profile", parent=self)
+
+        profileManager = ProfileManager(plot=self, parent=self.profileToolBar)
+        profileManager.setProfileWindowClass(CustomProfileWindow)
+        profileManager.setItemType(image=True)
+        profileManager.setActiveItemTracking(True)
+
+        roiManager = profileManager.getRoiManager()
+        roiManager.sigInteractiveRoiCreated.connect(self.configureRois)
+
+        horizontalLineAction = profileManager.createProfileAction(
+            ProfileImageHorizontalLineROI, parent=self.profileToolBar
+        )
+        verticalLineAction = profileManager.createProfileAction(
+            ProfileImageVerticalLineROI, parent=self.profileToolBar
+        )
+        crossLineAction = profileManager.createProfileAction(
+            ProfileImageCrossROI, parent=self.profileToolBar
+        )
+        freeLineAction = profileManager.createProfileAction(
+            ProfileImageLineROI, parent=self.profileToolBar
+        )
+        cleanAction = profileManager.createClearAction(self.profileToolBar)
+        editorAction = profileManager.createEditorAction(self.profileToolBar)
+
+        self.profileToolBar.addAction(horizontalLineAction)
+        self.profileToolBar.addAction(verticalLineAction)
+        self.profileToolBar.addAction(crossLineAction)
+        self.profileToolBar.addAction(freeLineAction)
+        self.profileToolBar.addAction(cleanAction)
+        self.profileToolBar.addAction(editorAction)
+
+        self.profileToolBar.setVisible(False)
+        self.addToolBar(self.profileToolBar)
+
+    def setProfileToolBarVisibility(self):
+        imageDataItems = [
+            item for item in self.getItems() if isinstance(item, items.ImageData)
+        ]
+        if imageDataItems:
+            self.profileToolBar.setVisible(True)
         else:
-            self.ratio = QGuiApplication.primaryScreen().devicePixelRatio()
+            self.profileToolBar.setVisible(False)
 
-        self.plotArea = self.getWidgetHandle()
-
-        self.sigPlotSignal.connect(self.plotEvent)
-
-    def plotEvent(self, event):
-        if event["event"] == "mouseMoved":
-            x, y = event["x"], event["y"]
-            xPixel, yPixel = event["xpixel"], event["ypixel"]
-            self.updateStatusBar(x, y, xPixel, yPixel)
-
-    def updateStatusBar(self, x, y, xPixel, yPixel):
-        selectedItems = self.getItems()
-
-        if not selectedItems:
-            return
-
-        distInPixels = (self.SNAP_THRESHOLD_DIST * self.ratio) ** 2
-
-        # TODO: Add the intensity for images.
-        for item in selectedItems:
-            if isinstance(item, Curve):
-                messageFormat = "X: {:g}    Y: {:.3g}"
-            elif isinstance(item, ImageData):
-                messageFormat = "X: {:g}    Y: {:g}"
-                continue
-
-            xArray = item.getXData(copy=False)
-            yArray = item.getYData(copy=False)
-
-            closestIndex = np.argmin(pow(xArray - x, 2) + pow(yArray - y, 2))
-
-            xClosest = xArray[closestIndex]
-            yClosest = yArray[closestIndex]
-
-            axis = item.getYAxis()
-
-            closestInPixels = self.dataToPixel(xClosest, yClosest, axis=axis)
-            if closestInPixels is not None:
-                xDistInPixels = closestInPixels[0] - xPixel
-                yDistInPixels = closestInPixels[1] - yPixel
-                curveDistInPixels = xDistInPixels ** 2 + yDistInPixels ** 2
-
-                if curveDistInPixels <= distInPixels:
-                    # If close enough, snap to data point coordinates.
-                    x, y = xClosest, yClosest
-                    distInPixels = curveDistInPixels
-
-        message = messageFormat.format(x, y)
-        self.window().statusBar().showMessage(message)
+    @staticmethod
+    def configureRois(roi):
+        roi.setName("")
+        roi.setProfileMethod("sum")
 
     def reset(self):
         self.clear()
@@ -160,78 +176,11 @@ class BasePlotWidget(PlotWidget):  # pylint: disable=too-many-instance-attribute
         self.setGraphYLabel("Y")
         self.setGraphYLimits(0, 100)
 
-    def addCurve(self, x, y, **kwargs):  # pylint: disable=arguments-differ
-        super(BasePlotWidget, self).addCurve(x, y, **kwargs)
-        self.addLegend()
 
-    def replot(self):
-        self.addLegend()
-        super(BasePlotWidget, self).replot()
-
-    def addLegend(self):
-        """Add the legend to the Matplotlib axis."""
-        images = self.getAllImages()
-        curves = self.getAllCurves()
-
-        if not curves or images:
-            legend = self.plotArea.ax.legend(handles=[])
-            legend.set_visible(False)
-            return
-
-        handles = list()
-        for curve in curves:
-            label = curve.getLegend()
-            style = curve.getCurrentStyle()
-            marker = style.getSymbol()
-            color = style.getColor()
-            lineStyle = style.getLineStyle()
-            handle = mlines.Line2D(
-                [], [], ls=lineStyle, color=color, label=label, marker=marker
-            )
-            handles.append(handle)
-        self.plotArea.ax.legend(handles=handles, fancybox=False)
-
-
-class ProfileWindow(BasePlotWidget):
+class MainPlotWidget(CustomPlotWidget):
     def __init__(self, parent=None, **kwargs):
-        super(ProfileWindow, self).__init__(parent=parent, **kwargs)
-
-        self.setWindowTitle(str())
-
-        if sys.platform == "darwin":
-            self.setIconSize(QSize(24, 24))
-
-
-class MainPlotWidget(BasePlotWidget):
-    def __init__(self, parent=None, **kwargs):
-        super(MainPlotWidget, self).__init__(parent=parent, **kwargs)
+        super().__init__(parent=parent, **kwargs)
         self.profileWindow = ProfileWindow()
-
-        # Instantiate the profile manager.
-        # profileManager = manager.ProfileManager(self, self)
-        # profileManager.setItemType(image=True)
-        # profileManager.setActiveItemTracking(True)
-
-        # profileWindow = ProfileWindow()
-        # self.profileToolBar = ProfileToolBar(
-        #     parent=self, plot=self, profileWindow=profileWindow
-        # )
-        # self.profileToolBar.clear()
-        # for action in profileManager.createImageActions(self.profileToolBar):
-        #     self.profileToolBar.addAction(action)
-
-        # action = profileManager.createClearAction(self.profileToolBar)
-        # self.profileToolBar.addAction(action)
-        # action = profileManager.createEditorAction(self.profileToolBar)
-        # self.profileToolBar.addAction(action)
-
-        # self.removeToolBar(self.outputToolBar)
-        # self.addToolBar(self.profileToolBar)
-        # self.addToolBar(self.outputToolBar)
-        # self.outputToolBar.show()
-
-        if sys.platform == "darwin":
-            self.setIconSize(QSize(24, 24))
 
         # Create QAction for the context menu once for all.
         self.zoomBackAction = ZoomBackAction(plot=self, parent=self)
@@ -268,3 +217,35 @@ class MainPlotWidget(BasePlotWidget):
         # plot area, and thus needs to be converted.
         globalPosition = self.plotArea.mapToGlobal(pos)
         menu.exec_(globalPosition)
+
+    def addCurve(self, x, y, **kwargs):  # pylint: disable=arguments-differ
+        super().addCurve(x, y, **kwargs)
+        self.addLegend()
+
+    def replot(self):
+        self.addLegend()
+        super().replot()
+
+    def addLegend(self):
+        """Add the legend to the Matplotlib axis."""
+        images = self.getAllImages()
+        curves = self.getAllCurves()
+
+        if not curves or images:
+            legend = self.plotArea.ax.legend(handles=[])
+            legend.set_visible(False)
+            return
+
+        handles = list()
+        for curve in curves:
+            label = curve.getLegend()
+            style = curve.getCurrentStyle()
+            marker = style.getSymbol()
+            color = style.getColor()
+            lineStyle = style.getLineStyle()
+            handle = mlines.Line2D(
+                [], [], ls=lineStyle, color=color, label=label, marker=marker
+            )
+            handles.append(handle)
+
+        self.plotArea.ax.legend(handles=handles, fancybox=False)
