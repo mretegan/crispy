a659b0c5fdfcd3224c088fae5fa635ebc260fbc7
Marius Retegan <marius.s.retegan@gmail.com>
2020-11-13 09:51:06 +0100

    Update version number

--- crispy/__init__.py
+++ crispy/__init__.py
@@ -8,7 +8,7 @@
 # For further information, see https://github.com/mretegan/crispy #
 ###################################################################
 """This is the crispy package."""
-__version__ = "2020.1rc1"
+__version__ = "2020.1rc0"
 
 import os
 import sys
