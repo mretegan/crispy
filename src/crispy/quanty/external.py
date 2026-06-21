2cdf72a67a757f7824b0498ad7946c52a4ced9f9
Marius Retegan <marius.s.retegan@gmail.com>
2026-06-21 11:38:26 +0700

    Add editable flags for the title in ExternalData class

--- src/crispy/quanty/external.py
+++ src/crispy/quanty/external.py
@@ -1,5 +1,7 @@
 import logging
 
+from silx.gui.qt import Qt
+
 from crispy.items import SelectableItem
 
 logger = logging.getLogger(__name__)
@@ -10,6 +12,14 @@ class ExternalData(SelectableItem):
         super().__init__(parent=parent, name=name)
         self.raw = raw
         self.enable()
+
+    def flags(self, column):
+        # Allow the title (column 0) to be edited; the name setter emits
+        # dataChanged, which replots and refreshes the legend.
+        flags = super().flags(column)
+        if column == 0:
+            return flags | Qt.ItemIsEditable
+        return flags
 
     def plot(self, plotWidget):
         index = self.childPosition()
